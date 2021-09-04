f6f6365d6a149cbbb49331e94068e7a551585cac
Nathan Wood <nathanwcad@gmail.com>
2021-09-03 22:26:27 -0400

    documentation

--- 02/TwoValueExchange.py
+++ 02/TwoValueExchange.py
@@ -1,4 +1,20 @@
 import math 
+"""
+This function exchanges two variables for each other
+
+Parameters
+----------
+    binit : float
+        This is simply b, but used for assertion testing
+    ainit : float
+        This should be a, but used only for assertion testing 
+    a : float
+        input 1
+    b : float
+        input 2 
+    temp : float
+        our placeholder to store numbers without losing them permanently 
+"""
 
 def ExchangeTwoVariable(a,b): 
     binit = b       # for assertion test
--- Euler/euler.py
+++ Euler/euler.py
@@ -2,20 +2,52 @@ import math
 import numpy as np
 import matplotlib as plt 
 
+"""
+This function performs the euler numerical approximation for differntial equations:
+
+        given dy/dx, y(x0) = y0, and a "step size" for iterations
+
+            y = y0 + h * f(x0,y0)
+Parameters
+-----------
+    xinit : float
+        our initial x value
+
+    yinit : float
+
+    xmax : float
+        our desired x value 
+    
+    stepSize : float : >= 0 
+        our step size for iteration, theoretically defined as h in textbooks 
+
+    diffFunc : equation
+        our differential equation 
+
+--------------
+End Parameters
+
+Yields
+------
+    Final approximation at xmax  
+
+"""
 
 def EulerApproximation(xinit,yinit,stepSize,diffFunc,xmax):
     for i in range(xmax): 
         ynew = yinit + stepSize * diffFunc      # performs the euler approximation y = y0 + h * f(x,y) 
         yinit = ynew                            # assigns new y for iteration 
         xinit = xinit + stepSize                # assigns new x for iteration
-    print("Final Y Approximation at x = %s : %s" %(xmax,yinit))     # prints new value 
-                
+        yfinal = ynew
+    print("Final Y Approximation at x = %s : %s" %(xmax,yfinal))     # prints new value 
+    return yfinal
+
 
 xmax=30
 xinit = 3
 yinit = 10 
-stepSize = 0.12
-diffFunc = np.sin(yinit) + np.cos(xinit**2)
+stepSize = 0.01
+diffFunc = np.sin(yinit) + np.cos(xinit**2) + xinit**3
 
 
 EulerApproximation(xinit,yinit,stepSize,diffFunc,xmax)
